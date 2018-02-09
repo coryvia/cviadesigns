1c72dd6c006bb49ce8372f3ebaeecab08e221010
AIORG\cvia <cvia@egov.com>
2018-02-09 13:06:10 -0500

    2

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	
+	$('#contact .col1 h2 a').mouseenter(function() {
+		$(this).html('cviadesigns@gmail.com');
+	});
+
+	$('#contact .col1 h2 a').mouseleave(function() {
+		$(this).html('Email Me');
+	});
+	
+	$('#contact .col2 h2 a').mouseenter(function() {
+		$(this).html('"Fill me in!"');
+	});
+
+	$('#contact .col2 h2 a').mouseleave(function() {
+		$(this).html('Contact Form');
+	});
+	
+	$('#contact .col3 h2 a').mouseenter(function() {
+		$(this).html('Let\'s Connect');
+	});
+
+	$('#contact .col3 h2 a').mouseleave(function() {
+		$(this).html('LinkedIn');
+	});
+});
